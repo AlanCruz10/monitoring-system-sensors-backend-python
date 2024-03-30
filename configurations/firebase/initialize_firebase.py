a7a72d15cc6984a4fcafbd12ddc2963870d47462
Alan George Trinidad Cruz <100612234+AlanCruz10@users.noreply.github.com>
2024-03-30 14:05:15 -0600

    Feature 001 mss cf (#3)
    * [ADD] Add configuration firebase and routes, add credentials to file gitignore
    
    * [UPDATE] update name json and add route sensors
    
    ---------
    
    Co-authored-by: AlanCruz10 <exalan30@gmail.com>

--- configurations/firebase/initialize_firebase.py
+++ configurations/firebase/initialize_firebase.py
@@ -4,14 +4,14 @@ import os
 
 
 def initialize_firebase():
-    # target = "configuration_firebase.json"
+    # target = "credential_firebase.json"
     # initial_dir = 'C:\\'
     # path = ''
     # for root, _, files in os.walk(initial_dir):
     #     if target in files:
     #         path = os.path.join(root, target)
     #         break
-    cred = credentials.Certificate('configurations/firebase/credentials/configuration_firebase.json')
+    cred = credentials.Certificate('configurations/firebase/credentials/credential_firebase.json')
     initialize_app = firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://monitoring-system-87637-default-rtdb.firebaseio.com/'})
     return initialize_app
--- configurations/firebase/routes/routes_firebase.py
+++ configurations/firebase/routes/routes_firebase.py
@@ -6,7 +6,8 @@ initialize_firebase.initialize_firebase()
 list_routes = {
     "dht11_humidity": db.reference("/sensor/dht11/humidity"),
     "dht11_temperature": db.reference("/sensor/dht11/temperature"),
-    "ds18b20_temperature": db.reference("/sensor/ds18b20/temperature")
+    "ds18b20_temperature": db.reference("/sensor/ds18b20/temperature"),
+    "sensors": db.reference("/sensor")
 }
 
 
